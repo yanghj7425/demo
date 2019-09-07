9ffd6f9e770b95dd5a00d460bc3f6021dff08a42
H.J.Yang <1501587673@qq.com>
2019-09-07 15:06:07 +0800

    delete useless file

--- order_service/src/main/java/com/yhj/order_service/service/ProductOrderService.java
+++
@@ -1,9 +0,0 @@
-package com.yhj.order_service.service;
-
-import com.yhj.order_service.domain.ProductOrder;
-
-public interface ProductOrderService {
-    ProductOrder save(Integer userId, Integer productId);
-
-    Object list();
-}
